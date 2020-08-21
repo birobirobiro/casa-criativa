dd560c214cc34a78bde85283b105879d318f5df1
João Inácio Neto <birobirobiro.dev@gmail.com>
2020-08-21 01:22:37 -0300

    Aula 3 finalizada :rocket:

--- scripts.js
+++ scripts.js
@@ -3,4 +3,14 @@
        .querySelector("#modal")
        .classList
        .toggle("hide")
+
+       document
+        .querySelector("body")
+        .classList
+        .toggle("hideScroll")
+
+        document
+            .querySelector("#modal")
+            .classList
+            .toggle("addScroll")
   }
